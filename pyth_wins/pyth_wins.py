c5ff7b53dc721a82ac0d0e659a68635f602f0d13
Eddie Cai <emcai@umich.edu>
2021-02-28 14:57:36 -0500

    Updated latest pyth_wins.py

--- pyth_wins/pyth_wins.py
+++ pyth_wins/pyth_wins.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-def load_data(file_name):
+def load_wins(file_name):
 	wins = {}
 	with open(file_name) as csvfile:
 		reader = csv.reader(csvfile)
@@ -18,11 +18,25 @@ def load_data(file_name):
 				wins[row[2]] += float(row[3])
 	return wins
 
+def load_ratings(file_name):
+    sp_ratings = {}
+    with open(file_name) as csvfile:
+        reader = csv.reader(csvfile)
+        csvfile.readline()
+        for row in reader:
+            sp_ratings[row[0]] = float(row[1])
+    return sp_ratings
+
 def main():
-	if len(sys.argv) != 2:
-		print("Usage: <model results file>")
+	if len(sys.argv) != 3:
+		print("Usage: <model results file> <SP+ ratings file>")
 		exit()
-	print(load_data(sys.argv[1]))
+	wins = load_wins(sys.argv[1])
+	ratings = load_ratings(sys.argv[2])
+
+	for team in model:
+		if team in ratings:
+			print(team + " " + str(model[team]))
 
 if __name__ == '__main__':
 	main()
